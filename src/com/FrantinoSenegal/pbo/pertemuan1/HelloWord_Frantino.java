69f4e42ed4fae4b1e5fefe5324ebf69effaa0748
FrantinoSenegal <131945387+FrantinoSenegal@users.noreply.github.com>
2023-05-01 19:22:47 +0700

    Add files via upload

---
+++ src/com/FrantinoSenegal/pbo/pertemuan1/HelloWord_Frantino.java
@@ -0,0 +1,7 @@
+package com.FrantinoSenegal.pbo.pertemuan1;
+
+public class HelloWord_Frantino {
+    public static void main(String[] args) {
+        System.out.println("Assalamualaikum wr.wb");
+    }
+}
